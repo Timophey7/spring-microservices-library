7d4b1beee102a9ccc3ead7831d3c3c990ba85127
Timpohey7 <timopheyonisenko@gmail.com>
2024-05-19 14:00:43 +0300

    second commit

--- cart-service/src/main/java/com/cartservice/CartServiceApplication.java
+++ cart-service/src/main/java/com/cartservice/CartServiceApplication.java
@@ -1,11 +1,8 @@
 package com.cartservice;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
@@ -14,9 +11,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class CartServiceApplication {
 
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.configure()
-                .directory("./")
-                .load();
         SpringApplication.run(CartServiceApplication.class, args);
     }
 
--- cart-service/src/main/resources/liquibase/changesets/003.create-cartItem-table.sql
+++ cart-service/src/main/resources/liquibase/changesets/003.create-cartItem-table.sql
@@ -16,3 +16,6 @@ alter table cart_items add constraint FKpcttvuq4mxppo8sxggjtn5i2c foreign key (c
 -- changeset liquibase/changesets/003.create-cartItmes-table.sql::add-fk-product-id::includeAll
 
 alter table cart_items add constraint FK1re40cjegsfvw58xrkdp6bac6 foreign key (product_id) references products (product_id);
+
+
+
